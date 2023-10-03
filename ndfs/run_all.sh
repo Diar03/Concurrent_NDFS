0613c3b5ea37415bb0012001d12b51d8cb12be18
Razvan Wist <razvan.wist@gmail.com>
2023-10-03 16:54:27 +0200

    fix script to display nodes

--- ndfs/run_all.sh
+++ ndfs/run_all.sh
@@ -15,13 +15,13 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 # Define table formatting characters
-horizontal_line="========================================================================================================="
-table_line="+-------------------------------------------------------------------------------------------------------+"
+horizontal_line="================================================================================================================================================"
+table_line="+----------------------------------------------------------------------------------------------------------------------------------------------+"
 
 # Print the table header with styling and borders
 echo "$horizontal_line"
 echo "$table_line"
-printf "| ${bold}%-30s${normal} | ${bold}%-20s${normal} | ${bold}%-20s${normal} | ${bold}%-22s${normal} |\n" "Input File" "Model" "Num Threads" "Value"
+printf "| ${bold}%-30s${normal} | ${bold}%-20s${normal} | ${bold}%-12s${normal} | ${bold}%-15s${normal} | ${bold}%-12s${normal} | ${bold}%-10s${normal} | ${bold}%-10s${normal} | ${bold}%-10s${normal} |\n" "Input File" "Model" "Num Threads" "Accepting Cycle" "Value" "Cyan" "Blue" "Red"
 echo "$table_line"
 
 # Enumerate over files in directory and execute the command for each
@@ -29,8 +29,19 @@ for filename in ./input/*; do
     output=$(./bin/ndfs.sh "$filename" "$model" "$numthreads")
     # Extract the relevant values from the output
     input_file=$(echo "$output" | grep -o "Graph .*" | cut -d ' ' -f 2)
+    cyan_nr=$(echo "$output" | grep -o "cyan .*" | cut -d ' ' -f 2)
+    blue_nr=$(echo "$output" | grep -o "blue .*" | cut -d ' ' -f 2)
+    red_nr=$(echo "$output" | grep -o "red .*" | cut -d ' ' -f 2)
+    accept_state=$(echo "$output" | grep -o "does .*" | cut -d ' ' -f 2)
+
+    if [ "$accept_state" == "not" ]; then
+        accept_state="NO"
+    else
+        accept_state="YES"
+    fi
+
     value=$(echo "$output" | grep -o "took .*" | cut -d ' ' -f 2)
     # Print the values in a table format with borders
-    printf "| %-30s | %-20s | %-20s | %-20s ms|\n" "$input_file" "$model" "$numthreads" "$value"
+    printf "| %-30s | %-20s | %-12s | %-15s | %-9s ms | ${bold}%-10s${normal} | ${bold}%-10s${normal} | ${bold}%-10s${normal} |\n" "$input_file" "$model" "$numthreads" "$accept_state" "$value" "$cyan_nr" "$blue_nr" "$red_nr"
     echo "$table_line"
 done
--- ndfs/src/ndfs/mcndfs_alg3_naive/Color.java
+++ ndfs/src/ndfs/mcndfs_alg3_naive/Color.java
@@ -5,5 +5,5 @@ package ndfs.mcndfs_alg3_naive;
  */
 public enum Color {
 
-    CYAN, WHITE, RED, BLUE
+    CYAN, WHITE, RED, BLUE, PINK
 }
